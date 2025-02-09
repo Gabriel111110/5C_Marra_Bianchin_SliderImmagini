1d4eadff21b81998435c4be2ebc417bb20ac8e6a
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-02-09 14:55:06 +0100

    fatta la parte del navigator e fatta la parte del reindirizzamento delle pagine (SPA)

---
+++ public/index.js
@@ -0,0 +1,2 @@
+import { createNavigator } from "./navigator.js";
+const navigator = createNavigator(document.querySelector("#container"));
---
+++ public/public.js
@@ -0,0 +1,5 @@
+const bottone = document.querySelector('#bottone');
+
+bottone.onclick=()=>{
+    alert('Ciao');
+}

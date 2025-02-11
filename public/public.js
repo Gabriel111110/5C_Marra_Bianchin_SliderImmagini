80fcff53c4ed674fc4fb77708fc3199c8914c578
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-02-11 09:16:25 +0100

    creazione cartella files per immagini

--- public/public.js
+++ public/public.js
@@ -1,15 +1,2 @@
 const vaiAdAdmin = document.querySelector('#goToAdmin');
 const vaiAPublic = document.querySelector('#goToPublic');
-
-
-
-
-
-//quando viene premuto vaiAdAdmin bisogna andare alla pagina di login
-
-
-vaiAdAdmin.onclick=()=>{
-    
-
-
-}
